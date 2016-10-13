03ca8d753732559bcfdc6e0ee8fa150b8dfe61e3
bombichm <matt@bombich.com>
2016-10-13 11:41:38 -0400

    Add files via upload

--- RBV2/Motor_Controls.ino
+++ RBV2/Motor_Controls.ino
@@ -1,135 +1,217 @@
 // These are the pin connections for the motor controller, they won't change.
-const byte A_EN = 2;    //right wheel
-const byte A_RPWM = 3;    //(forward)
-const byte A_DIS = 4;    
-const byte A_LPWM = 11;   //(reverse)
+#if (oldMotorController == false)
 
-const byte B_EN = 8;   //left wheel
-const byte B_RPWM = 9;   //(forward)
-const byte B_DIS = 7;   
-const byte B_LPWM = 10;  //(reverse)
-const byte goButton = 14; //this button will execute our functions
-
-
+  const byte A_EN = 2;    //right wheel
+  const byte A_RPWM = 3;    //(forward)
+  const byte A_DIS = 4;    
+  const byte A_LPWM = 11;   //(reverse)
+  
+  const byte B_EN = 8;   //left wheel
+  const byte B_RPWM = 9;   //(forward)
+  const byte B_DIS = 7;   
+  const byte B_LPWM = 10;  //(reverse)
+  const byte goButton = 14; //this button will execute our functions
+  
   
   // Let's declare some things first.  Leave these ones alone...
-#define pinMode(A_EN, OUTPUT);
-#define pinMode(A_DIS, OUTPUT);
-#define pinMode(B_EN, OUTPUT);
-#define pinMode(B_DIS, OUTPUT);
-  /** PWM pin configurate */
-#define pinMode(A_RPWM, OUTPUT);
-#define pinMode(A_LPWM, OUTPUT);
-#define pinMode(B_RPWM, OUTPUT);
-#define pinMode(B_LPWM, OUTPUT);
-#define pinMode(goButton, INPUT);
+  #define pinMode(A_EN, OUTPUT);
+  #define pinMode(A_DIS, OUTPUT);
+  #define pinMode(B_EN, OUTPUT);
+  #define pinMode(B_DIS, OUTPUT);
+    /** PWM pin configurate */
+  #define pinMode(A_RPWM, OUTPUT);
+  #define pinMode(A_LPWM, OUTPUT);
+  #define pinMode(B_RPWM, OUTPUT);
+  #define pinMode(B_LPWM, OUTPUT);
+  #define pinMode(goButton, INPUT);
+    
   
-
-void forward (int duration, byte speed)
-{
-  Serial.println("Forward");
-  leftMotorF(speed);
-  rightMotorF(speed);
-  delay (duration);
-  stop();
-}
-
-void reverse (int duration, byte speed)
-{
-  Serial.println ("Reverse");
-  leftMotorR(speed);
-  rightMotorR(speed);
-  delay (duration);
-  stop();
-}
-
-void stop()
-{
-  Serial.println ("Stop");
-  digitalWrite(A_EN, LOW);
-  digitalWrite(B_EN, LOW);
-  digitalWrite(A_RPWM, HIGH);
-  digitalWrite(A_LPWM, HIGH);
-  digitalWrite(B_RPWM, HIGH);
-  digitalWrite(B_LPWM, HIGH);
-  delay(100);
-  digitalWrite(A_EN, HIGH);
-  digitalWrite(B_EN, HIGH);
-}
-
-void turnRight(int duration, int speed)   //add gyro code later angle = seconds of turn
-{
-  Serial.println ("turnRight");
-  leftMotorF (speed);
-  rightMotorR (speed);
-  delay(duration);
-  stop();
-}
-
-void turnLeft(int duration, int speed)   //add gyro code later angle = seconds of turn
-{
-  Serial.println ("turnLeft");
-  leftMotorR (speed);
-  rightMotorF (speed);
-  delay(duration);
-  stop();
-}
-
-void leftMotorF(byte speed)
-{
-  Serial.println ("leftMotorF");
-  digitalWrite(A_RPWM, HIGH); //left motor forward
-  analogWrite(A_LPWM, 255-speed);
-}
-
-void rightMotorF (byte speed)
-{
-  Serial.println ("rightMotorF");
-  digitalWrite(B_LPWM, HIGH);  //right motor forward
-  analogWrite(B_RPWM, 255-speed);
-}
-
-void leftMotorR(byte speed)
-{
-  Serial.println ("leftMotorR");
-  analogWrite(A_RPWM, (255-speed)); //left motor reverse
-  digitalWrite(A_LPWM, HIGH);
-}
-
-void rightMotorR(byte speed)
-{
-  Serial.println ("rightMotorR");
-  digitalWrite(B_RPWM, HIGH);  //right motor reverse
-  analogWrite(B_LPWM, (255-speed));
-}
-
-void wait()
-{
-    while  (digitalRead(goButton) == LOW);
+  void forward (int duration, byte speed)
   {
+    Serial.println("Forward");
+    leftMotorF(speed);
+    rightMotorF(speed);
+    delay (duration);
     stop();
   }
-}
+  
+  void reverse (int duration, byte speed)
+  {
+    Serial.println ("Reverse");
+    leftMotorR(speed);
+    rightMotorR(speed);
+    delay (duration);
+    stop();
+  }
+  
+  void stop()
+  {
+    Serial.println ("Stop");
+    digitalWrite(A_EN, LOW);
+    digitalWrite(B_EN, LOW);
+    digitalWrite(A_RPWM, HIGH);
+    digitalWrite(A_LPWM, HIGH);
+    digitalWrite(B_RPWM, HIGH);
+    digitalWrite(B_LPWM, HIGH);
+    delay(100);
+    digitalWrite(A_EN, HIGH);
+    digitalWrite(B_EN, HIGH);
+  }
+  
+  void turnRight(int duration, int speed)   //add gyro code later angle = seconds of turn
+  {
+    Serial.println ("turnRight");
+    leftMotorF (speed);
+    rightMotorR (speed);
+    delay(duration);
+    stop();
+  }
+  
+  void turnLeft(int duration, int speed)   //add gyro code later angle = seconds of turn
+  {
+    Serial.println ("turnLeft");
+    leftMotorR (speed);
+    rightMotorF (speed);
+    delay(duration);
+    stop();
+  }
+  
+  void leftMotorF(byte speed)
+  {
+    Serial.println ("leftMotorF");
+    digitalWrite(A_RPWM, HIGH); //left motor forward
+    analogWrite(A_LPWM, 255-speed);
+  }
+  
+  void rightMotorF (byte speed)
+  {
+    Serial.println ("rightMotorF");
+    digitalWrite(B_LPWM, HIGH);  //right motor forward
+    analogWrite(B_RPWM, 255-speed);
+  }
+  
+  void leftMotorR(byte speed)
+  {
+    Serial.println ("leftMotorR");
+    analogWrite(A_RPWM, (255-speed)); //left motor reverse
+    digitalWrite(A_LPWM, HIGH);
+  }
+  
+  void rightMotorR(byte speed)
+  {
+    Serial.println ("rightMotorR");
+    digitalWrite(B_RPWM, HIGH);  //right motor reverse
+    analogWrite(B_LPWM, (255-speed));
+  }
+  
+  void wait()
+  {
+      while  (digitalRead(goButton) == LOW);
+    {
+      stop();
+    }
+  }
+  
+  
+  /*
+  Channel A
+  EN pin 2
+  RPWM pin 3
+  DIS pin 4
+  LPWM pin 11
+  
+  Channel B
+  EN pin 8
+  RPWM pin 9
+  DIS pin 7
+  LPWM pin 10
+  
+  Rotate forward: EN = HIGH, RPWM = PWM, LPWM = HIGH, DIS = vacant
+  Rotate reverse: EN = HIGH, RPWM = HIGH, LPWM = PWM, DIS = vacant
+  Stop and brake: EN = HIGH, RPWM = HIGH, LPWM = HIGH, DIS = vacant
+  Stop but not brake: EN = 0, RPWM = HIGH, LPWM = HIGH, DIS = vacant
+  Prohibit : EN = X, RPWM = X, LPWM = X, DIS = HIGH
+  
+  */
 
 
-/*
-Channel A
-EN pin 2
-RPWM pin 3
-DIS pin 4
-LPWM pin 11
 
-Channel B
-EN pin 8
-RPWM pin 9
-DIS pin 7
-LPWM pin 10
+#else  //old motor controller code
 
-Rotate forward: EN = HIGH, RPWM = PWM, LPWM = HIGH, DIS = vacant
-Rotate reverse: EN = HIGH, RPWM = HIGH, LPWM = PWM, DIS = vacant
-Stop and brake: EN = HIGH, RPWM = HIGH, LPWM = HIGH, DIS = vacant
-Stop but not brake: EN = 0, RPWM = HIGH, LPWM = HIGH, DIS = vacant
-Prohibit : EN = X, RPWM = X, LPWM = X, DIS = HIGH
+    //declaring the pins for the IN pins on the L298N
+  const int rightEnablePin = 3;  //Enable A
+  const int rightForwardPin = 4; //IN2
+  const int rightBackwardPin = 2; //IN1
+  const int leftEnablePin = 5;  //Enable B
+  const int leftBackwardPin = 7; //IN4
+  const int leftForwardPin = 8; //IN3
+    
+  //Stating that the pins are OUTPUT
+  pinMode(rightEnablePin, OUTPUT);
+  pinMode(rightForwardPin, OUTPUT);
+  pinMode(rightBackwardPin, OUTPUT);
+  pinMode(leftEnablePin, OUTPUT);
+  pinMode(leftForwardPin, OUTPUT);
+  pinMode(leftBackwardPin, OUTPUT);
+  
+  void forward(int duration, byte speed){
+    Serial.println("Drive Forward");
+    
+    digitalWrite(rightForwardPin, HIGH);
+    digitalWrite(rightBackwardPin, LOW);
+    digitalWrite(leftForwardPin, HIGH);
+    digitalWrite(leftBackwardPin, LOW);
+    analogWrite(rightEnablePin, speed); 
+    analogWrite(leftEnablePin, speed);
+    delay(duration);
+  }
+    
+  //Setting the wheels to go backward by setting the backward pins to HIGH
+  void reverse(int duration, byte speed){
+    Serial.println("Drive Backwards");
+    digitalWrite(rightForwardPin, LOW);
+    digitalWrite(rightBackwardPin, HIGH);
+    digitalWrite(leftForwardPin, LOW);
+    digitalWrite(leftBackwardPin, HIGH);
+    analogWrite(rightEnablePin, speed); 
+    analogWrite(leftEnablePin, speed);
+    delay(duration);
+  }
+  
+  //Setting the wheels to go right by setting the rightBackwardPin and leftForwardPin to HIGH
+  void turnRight(int duration, byte speed){
+    Serial.println("Turn Right");
+    digitalWrite(rightForwardPin, LOW);
+    digitalWrite(rightBackwardPin, HIGH);
+    digitalWrite(leftForwardPin, HIGH);
+    digitalWrite(leftBackwardPin, LOW);
+    analogWrite(rightEnablePin, speed); 
+    analogWrite(leftEnablePin, speed);
+    delay(duration);
+  }
+  
+  //Setting the wheels to go left by setting the rightForwardPin and leftBackwardPin to HIGH
+  void turnLeft(int duration, byte speed){
+    Serial.println("Turn Left");
+    digitalWrite(rightForwardPin, HIGH);
+    digitalWrite(rightBackwardPin, LOW);
+    digitalWrite(leftForwardPin, LOW);
+    digitalWrite(leftBackwardPin, HIGH);
+    analogWrite(rightEnablePin, speed); 
+    analogWrite(leftEnablePin, speed);
+    delay(duration);
+  }
+  
+  //Setting the wheels to go stop by setting all the pins to LOW
+  void stop(){
+    Serial.println("Stop");
+    digitalWrite(rightEnablePin, LOW); 
+    digitalWrite(leftEnablePin, LOW);
+    digitalWrite(rightForwardPin, LOW);
+    digitalWrite(rightBackwardPin, LOW);
+    digitalWrite(leftForwardPin, LOW);
+    digitalWrite(leftBackwardPin, LOW);
+    delay(100);
+  }
 
-*/
-
+#endif
--- RBV2/RBV2.ino
+++ RBV2/RBV2.ino
@@ -21,33 +21,37 @@
 //******************************  ENTER YOUR FUNCTIONS HERE  *******************************************************************
 void setup()
 {
+  bool oldMotorController = true; //set this to true if your robot is using the old-style, L298 dual H-bridge motor controller
+  
   Serial.begin(115200); //Setup serial port for debugging our code
+  //  Look for button push, then execute loop
+  //wait(); //this function is waiting for a button push to make the robot go
+  //forward(1000, 100); 
+  // turnLeft(250, 100);
+  // reverse(2000, 100);
+  
+  //leftMotorF(150);
+  //rightMotorF(150);
+  // leftMotorF(50);
+  //delay(500);
+  //stop();
+  //leftMotorR(100);
+  //delay (1000);
+  //stop();
+  //  rightMotorR(2000, 150);
+  // for (int i = 0; i < 4; i++)
+  // {
+  // forward(1000, 100);
+  // turnRight(725, 100);
+  // }
 }
 
 
   
 void loop()
 {
-  //  Look for button push, then execute loop
-  wait(); //this function is waiting for a button push to make the robot go
-  forward(1000, 100); 
-// turnLeft(250, 100);
-// reverse(2000, 100);
 
-//leftMotorF(150);
-rightMotorF(150);
-// leftMotorF(50);
-//delay(500);
-//stop();
-//leftMotorR(100);
-//delay (1000);
-//stop();
-//  rightMotorR(2000, 150);
-// for (int i = 0; i < 4; i++)
-// {
-// forward(1000, 100);
-// turnRight(725, 100);
-// }
+//
 
 } 
 
