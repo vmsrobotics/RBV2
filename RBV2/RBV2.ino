6341ceef1ffa6b4cdd3de21b54894f9c6905c9da
vmsrobotics <vmscontrolfreaks@gmail.com>
2016-11-15 18:22:21 -0500

    11-15-16

--- RBV2/RBV2.ino
+++ RBV2/RBV2.ino
@@ -2,52 +2,59 @@ bool oldMotorController = true; //set this to true if your robot is using the ol
 
 //******************************  ENTER YOUR FUNCTIONS HERE  *******************************************************************
 
-void studentCode()
-{
+//void studentCode()
+//{
+//
+//
+//// Drive Straight until obstacle is reached
+//
+//sonarForwardAvoid(72,125);
+//
+////Turn right
+//sonarTurnRight(12, 90, 125, 1);
+//
+//// Drive Straight until obstacle is reached
+//sonarForwardAvoid(49, 125);
+//
+////Turn left
+//turnLeft(125,90);
+//
+//// Drive Straight until obstacle is reached
+//sonarForwardAvoid(38, 125);
+//
+////Turn right
+//sonarTurnRight(12, 90, 125, 1);
+//
+//// Drive Straight until obstacle is reached
+//sonarForwardAvoid(29, 125);
+//
+////Turn Right
+//sonarTurnRight(12, 90, 125, 1);
+//int blue = colorReadBlue();
+//int red = colorReadRed();
+//// Drive Straight until obstacle is reached where you will need to be close enough to read color
+//if (red > blue)
+//  {
+////turn left
+//}
+//else
+//{
+////turn right(90, 125)
+//}
+//
+//
+////Read color at obstacle, turn accordingly
+//
+//
+//
+//}
+//
 
 
-// Drive Straight until obstacle is reached
 
 
-//Turn right
+   
 
-
-// Drive Straight until obstacle is reached
-
-
-//Turn left
-
-
-// Drive Straight until obstacle is reached
-
-
-//Turn right
-
-
-// Drive Straight until obstacle is reached
-
-
-//Turn Right
-
-
-// Drive Straight until obstacle is reached where you will need to be close enough to read color
-
-
-//Read color at obstacle, turn accordingly
-
-//    Serial.print("red =  ");
-//    Serial.print(red);
-//    Serial.print("blue =  ");
-//    Serial.print(red);
-
-
-
-
-
-
-
-  
-}
 
 
   
--- RBV2/UltraSonic.ino
+++ RBV2/UltraSonic.ino
@@ -6,53 +6,51 @@
 const int trigPin = 5;  // Arduino pin tied to trigger pin on the ultrasonic sensor.
 const int echoPin = 6;  // Arduino pin tied to echo pin on the ultrasonic sensor.
 
-int distance;
 
 int sonar() 
 {
-  long duration;
-  int reading;
+    long duration;
+    int distance;
   
-  for (int x=0; x < 2; x++)
-  {
-    int oldReading = reading;
     digitalWrite(trigPin, LOW);  // Clears the trigPin
     delayMicroseconds(2);
     digitalWrite(trigPin, HIGH);  // Sets the trigPin on HIGH state for 10 micro seconds
     delayMicroseconds(10);
     digitalWrite(trigPin, LOW);
     duration = pulseIn(echoPin, HIGH);  // Reads the echoPin, returns the sound wave travel time in microseconds
-    reading= duration * 0.0067;  // Calculating the distance in inches
+    distance = duration * 0.0067;  // Calculating the distance in inches
     delay(25);
-    if (reading != 0) 
-      { 
-        (reading += oldReading) / 2; //averaging to smooth values
-      }
-    else
-      {
-        reading = oldReading;
-      }
-  }
-  distance = reading;
+    
+//    if (reading != 0) 
+//      { 
+//        (reading += oldReading) / 2; //averaging to smooth values
+//      }
+//    else
+//      {
+//        reading = oldReading;
+//      }
+//  }
+ 
   return distance;
 }//end int sonar()
   
 
 void sonarTurnRight(int bumpDistance, int turnDuration, byte speed, int turns) 
   { 
-    if (sonar())
+    int distance = sonar();
+    if (distance > 2)
     {
     Serial.println("Sonar Turn Right Initiated, Drive Forward, wait for bump");
     int counter = 0;
     leftMotorF(speed);
     rightMotorF(speed);
     Serial.println("Initial delay");
-    delay(50);
+//    delay(50);
 //    Serial.println("Initial delay complete");
     
     while (counter < turns)
       {
-        sonar();
+        distance = sonar();
         Serial.print("Sensing....");
               
         if (distance > bumpDistance)
@@ -85,26 +83,27 @@ void sonarTurnRight(int bumpDistance, int turnDuration, byte speed, int turns)
     
     else
     {
-      Serial.println("No Sonar Response");
+      Serial.println("No Sonar Response " + distance);
     }
   }//end sonarTurnRight
 
 
 void sonarSerial()
 {
+    
     int counter = 0;
     while (counter < 10000)
       {
-      sonar();
+      int distance = sonar();
       Serial.println(distance);
       counter++;
-      delay(50);
       }
 }//end sonarSerial
 
 void sonarForwardAvoid(int bumpDistance, byte speed) 
   {
-    if (sonar())
+    int distance = sonar();
+    if (distance > 2)
     {
     Serial.print("Sonar Forward Avoid Initiated, Drive Forward, stop at distance of ");
     Serial.println(bumpDistance);
@@ -116,7 +115,7 @@ void sonarForwardAvoid(int bumpDistance, byte speed)
     
     while (distance > bumpDistance)
       {
-        sonar();
+        distance = sonar();
         if (distance > bumpDistance)
         {
           Serial.print("Running Sonar Forward Avoid, distance not reached, distance detected ");
@@ -132,6 +131,6 @@ void sonarForwardAvoid(int bumpDistance, byte speed)
     }//end if (sonar())
     else
     {
-      Serial.println("No Sonar Response");
+      Serial.println("No Sonar Response " + distance);
     }
   }//end sonarForwardAvoid
